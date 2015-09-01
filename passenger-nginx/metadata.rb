c3fbe8e8e8e79e1e140a017e62495e4e68cdbccf
gfleury <gfleury@gmail.com>
2015-09-01 11:41:31 -0300

    Fix

--- passenger-nginx/metadata.rb
+++
@@ -1,9 +0,0 @@
-name             'passenger-nginx'
-maintainer       'Leonard Teo'
-maintainer_email 'leonard@ballistiq.com'
-license          'MIT'
-description      'Installs/Configures Passenger with Nginx'
-long_description 'Installs/Configures RVM, Ruby, Phusion Passenger (open source and Enterprise editions) with Nginx'
-version          '0.9.15'
-
-supports 'ubuntu'
